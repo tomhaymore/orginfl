707c29d89aa11c5cea1024b63808cef345d73c01
Thomas Haymore <thomas.haymore@gmail.com>
2011-08-18 17:48:32 -0700

    Don't use this commit

--- regulations/test_reg.py
+++ regulations/test_reg.py
@@ -5,11 +5,9 @@ dataPath = "/Users/thaymore/Documents/Research/CDRG/Data/regulations/regulations
 f = open(dataPath, 'r')
 i = 0
 
-lines = f.readlines(10024)
+lines = f.readlines(1024)
 
-for line in lines[0:100]:
-	line = line.split(',')
-	print line[7]
+print lines[0]
 
 #for line in f:
 #	i += 1
