92e8e0c78c9e1b3071d4c13511f561c033fd95e1
Thomas Haymore <thomas.haymore@gmail.com>
2011-08-18 17:48:40 -0700

    Revert "Don't use this commit"
    This reverts commit 707c29d89aa11c5cea1024b63808cef345d73c01.

--- regulations/test_reg.py
+++ regulations/test_reg.py
@@ -5,9 +5,11 @@ dataPath = "/Users/thaymore/Documents/Research/CDRG/Data/regulations/regulations
 f = open(dataPath, 'r')
 i = 0
 
-lines = f.readlines(1024)
+lines = f.readlines(10024)
 
-print lines[0]
+for line in lines[0:100]:
+	line = line.split(',')
+	print line[7]
 
 #for line in f:
 #	i += 1
